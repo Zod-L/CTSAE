1e1c795178845b60d15f9e7643fe016fc253708a
Yi Li <yi.li@dgx1.ligo-la.caltech.edu>
2024-02-25 12:57:30 -0600

    Update name of models

--- models.py
+++ models.py
@@ -22,7 +22,7 @@ from timm.models.registry import register_model
 
 
 @register_model
-def cnn_split224_4branch(pretrained=False, use_vae=False, **kwargs):
+def cnn(pretrained=False, use_vae=False, **kwargs):
     model = auto_encoder_cnn(patch_size=16, channel_ratio_encoder=2, channel_ratio_decoder=2, embed_dim=384, decode_embed=192, depth=12,
                        im_size=224, first_up=2, use_vae=use_vae, **kwargs)
     if pretrained:
@@ -33,7 +33,7 @@ def cnn_split224_4branch(pretrained=False, use_vae=False, **kwargs):
 
 
 @register_model
-def attn_split224_4branch(pretrained=False, use_vae=False, **kwargs):
+def vit(pretrained=False, use_vae=False, **kwargs):
     model = auto_encoder_vit(patch_size=16, channel_ratio_encoder=2, channel_ratio_decoder=2, embed_dim=384, decode_embed=192, depth=12,
                        im_size=224, first_up=2, use_vae=use_vae, **kwargs)
     if pretrained:
@@ -47,7 +47,7 @@ def attn_split224_4branch(pretrained=False, use_vae=False, **kwargs):
 
 
 @register_model
-def cls_attn_cnn_split224(pretrained=False, **kwargs):
+def conformer(pretrained=False, **kwargs):
     # model = auto_encoder(patch_size=16, channel_ratio=4, embed_dim=768, decode_embed=384, depth=12,
     #                   num_heads=6, mlp_ratio=4, qkv_bias=True, **kwargs)
     model = auto_encoder(patch_size=16, channel_ratio=2, embed_dim=384, decode_embed=192, depth=12,
@@ -67,7 +67,7 @@ def cls_attn_cnn_split224(pretrained=False, **kwargs):
 
 
 @register_model
-def cls_attn_cnn_split224_4branch(pretrained=False, **kwargs):
+def cnn_concat_attn(pretrained=False, **kwargs):
     model = auto_encoder_multi_branch(patch_size=16, channel_ratio=2, embed_dim=384, decode_embed=192, depth=12,
                       num_heads=6, mlp_ratio=2, qkv_bias=True, im_size=224, first_up=2, num_branch=4, **kwargs)
     if pretrained:
@@ -95,18 +95,18 @@ def cnn_split_attn(pretrained=False, use_vae=False, **kwargs):
 
 
 
-@register_model
-def cnn_attn_encoder_cnn_decoder_224(pretrained=False, use_vae=False, **kwargs):
-    model = cnn_attn_encoder_cnn_decoder(patch_size=16, channel_ratio=2, embed_dim=384, decode_embed=192, depth=12,
-                      num_heads=6, mlp_ratio=2, qkv_bias=True, im_size=224, first_up=2, num_branch=4, use_vae=use_vae, **kwargs)
-    if pretrained:
-        raise NotImplementedError
-    return model
+# @register_model
+# def cnn_attn_encoder_cnn_decoder_224(pretrained=False, use_vae=False, **kwargs):
+#     model = cnn_attn_encoder_cnn_decoder(patch_size=16, channel_ratio=2, embed_dim=384, decode_embed=192, depth=12,
+#                       num_heads=6, mlp_ratio=2, qkv_bias=True, im_size=224, first_up=2, num_branch=4, use_vae=use_vae, **kwargs)
+#     if pretrained:
+#         raise NotImplementedError
+#     return model
 
-@register_model
-def auto_encoder_no_comm_224(pretrained=False, use_vae=False, **kwargs):
-    model = auto_encoder_no_comm(patch_size=16, channel_ratio=2, embed_dim=384, decode_embed=192, depth=12,
-                      num_heads=6, mlp_ratio=2, qkv_bias=True, im_size=224, first_up=2, num_branch=4, use_vae=use_vae, **kwargs)
-    if pretrained:
-        raise NotImplementedError
-    return model
+# @register_model
+# def auto_encoder_no_comm_224(pretrained=False, use_vae=False, **kwargs):
+#     model = auto_encoder_no_comm(patch_size=16, channel_ratio=2, embed_dim=384, decode_embed=192, depth=12,
+#                       num_heads=6, mlp_ratio=2, qkv_bias=True, im_size=224, first_up=2, num_branch=4, use_vae=use_vae, **kwargs)
+#     if pretrained:
+#         raise NotImplementedError
+#     return model
